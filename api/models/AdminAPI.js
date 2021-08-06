7af530b7f4e5c5d2401178b198471b2928f29450
imodclub <70686000+imodclub@users.noreply.github.com>
2021-08-06 07:04:19 +0700

    update unique

--- api/models/AdminAPI.js
+++ api/models/AdminAPI.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const adminSchema = new Schema({
-  email: { type: String },
+  email: {
+    type: String,
+    index: { unique: true }
+  },
   password: { type: String },
 });
 
